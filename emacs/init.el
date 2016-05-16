6014eefa4b6f2cc4f4e68b4f984a1448abfd56ff
Bogdan Cornianu <cornianu.bogdan@gmail.com>
2016-05-16 21:09:37 +0300

    initial commit.

---
+++ emacs/init.el
@@ -0,0 +1,58 @@
+(custom-set-variables
+    '(ansi-color-names-vector ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+    '(column-number-mode t)
+    '(show-paren-mode t)
+    '(tool-bar-mode nil)
+    '(show-trailing-whitespace t)
+    '(ido-mode t)
+    '(menu-bar-mode nil)
+    '(scroll-bar-mode nil)
+    '(inhibit-startup-screen t)
+    '(large-file-warning-threshold nil)
+    '(python-shell-interpreter "python")
+    '(display-time-day-and-date t)
+    '(display-time-24hr-format t)
+    '(custom-enabled-themes (quote (wombat)))
+)
+
+(require 'package)
+(add-to-list 'package-archives
+	     '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(defun install-if-needed (package)
+  (unless (package-installed-p package)
+    (package-refresh-contents)
+    (package-install package)))
+
+(package-initialize)
+(package-refresh-contents)
+
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.sls?\\'" . yaml-mode))
+
+(setq package-list '(fill-column-indicator auto-complete flycheck less-css-mode markdown-mode jedi smartparens magit))
+
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(require 'auto-complete)
+(require 'auto-complete-config)
+(ac-config-default)
+(global-auto-complete-mode t)
+
+(require 'uniquify)
+(require 'ido)
+(ido-mode t)
+
+(require 'fill-column-indicator)
+(define-globalized-minor-mode
+ global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(global-fci-mode t)
+
+
